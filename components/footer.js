961125cf1af98dec633d95d381c02acd2d1dba3b
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-11-19 16:34:47 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Raiyan Yahya. inspired by Takuya Matsuyama&apos;s profile.
+      &copy; {new Date().getFullYear()} Raiyan Yahya. inspired by Takuya Matsuyama&apos;s <Link href="https://www.craftz.dog/" passHref={true}> profile </Link>
     </Box>
   )
 }
